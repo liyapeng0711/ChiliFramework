19ae438d0f325d1aa2b115100d4c2c8b1fc0c39c
Yapeng Li <liyapeng0711@163.com>
2019-11-15 18:01:58 +0800

    finish Ball Class

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -1,9 +1,33 @@
 #include "Ball.h"
 
-
-
-Ball::Ball()
+Ball::Ball(Vec2 center, Vec2 velocity)
+	:
+	center(center),
+	velocity(velocity)
 {
 }
 
+Rect Ball::GetRect() const
+{
+	return Rect::RectCenter(center, width, height);
+}
 
+void Ball::Draw(Graphics & gfx) const
+{
+	SpriteCodex::DrawBall(center, gfx);
+}
+
+void Ball::Update(float dt)
+{
+	center += velocity*dt;
+}
+
+void Ball::ReboundX()
+{
+	velocity.x = -velocity.x;
+}
+
+void Ball::ReboundY()
+{
+	velocity.y = -velocity.y;
+}
--- Engine/Ball.h
+++ Engine/Ball.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "Rect.h"
+#include "SpriteCodex.h"
+
 class Ball
 {
 public:
-	Ball(Vec2 center);
-
+	Ball(Vec2 center, Vec2 vlocity);
+	Rect GetRect()const;
+	void Draw(Graphics& gfx)const;
+	void Update(float dt);
+	void ReboundX();
+	void ReboundY();
 private:
 	Vec2 center;
-	static constexpr float width = 14;
-	static constexpr float height = 14;
+	static constexpr float width = 14.0f;
+	static constexpr float height = 14.0f;
+	Vec2 velocity;
 };
 
