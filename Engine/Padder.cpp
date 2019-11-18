64d0fe59e1df0f98004ff895d367752958475202
Yapeng Li <liyapeng0711@163.com>
2019-11-18 23:13:03 +0800

    Padder Class

--- Engine/Padder.cpp
+++ Engine/Padder.cpp
@@ -2,11 +2,55 @@
 
 
 
-Padder::Padder()
+Padder::Padder(Rect rect)
+	:
+	rect(rect)
 {
 }
 
-
-Padder::~Padder()
+void Padder::Update(Keyboard& kbd, float dt)
 {
+	if (kbd.KeyIsPressed(VK_LEFT))
+	{
+		rect = Rect(rect.GetLeftUp() - Vec2(speed*dt, 0), rect.GetRightDown() - Vec2(speed*dt, 0));
+	}
+	if (kbd.KeyIsPressed(VK_RIGHT))
+	{
+		rect = Rect(rect.GetLeftUp() + Vec2(speed*dt, 0), rect.GetRightDown() + Vec2(speed*dt, 0));
+	}
+}
+
+void Padder::LimitInRect(Rect rect_)
+{
+	const float left = rect_.GetLeftUp().x;
+	const float right = rect_.GetRightDown().x;
+	const float up = rect_.GetLeftUp().y;
+	const float down = rect_.GetRightDown().y;
+	if (rect.GetLeftUp().x < left)
+	{
+		rect.SetLeft(left);
+	}
+	else if (rect.GetRightDown().x > right)
+	{
+		rect.SetRight(right);
+	}
+	if (rect.GetLeftUp().y < up)
+	{
+		rect.SetUp(up);
+	}
+	else if (rect.GetRightDown().y > down)
+	{
+		rect.SetDown(down);
+	}
+}
+
+void Padder::Draw(Graphics & gfx) const
+{
+	gfx.DrawRect(rect, paddingColor);
+	gfx.DrawRect(Rect(rect.GetLeftUp() + Vec2(paddingWidth, 0), rect.GetRightDown() - Vec2(paddingWidth, 0)), color);
+}
+
+Rect Padder::GetRect() const
+{
+	return rect;
 }
--- Engine/Padder.h
+++ Engine/Padder.h
@@ -1,8 +1,23 @@
 #pragma once
+#include"Rect.h"
+#include"Colors.h"
+#include"Graphics.h"
+#include"Keyboard.h"
 class Padder
 {
 public:
-	Padder();
-	~Padder();
+	Padder() = default;
+	Padder(Rect rect);
+	void Update(Keyboard& kbd, float dt);
+	void LimitInRect(Rect rect_);
+	void Draw(Graphics& gfx)const;
+	Rect GetRect()const;
+
+private:
+	Rect rect;//whole rect
+	static constexpr Color color=Colors::Green;//center color
+	static constexpr float paddingWidth = 20.0f;//toward inside
+	static constexpr Color paddingColor = Colors::Red;//two sides color
+	float speed = 200;
 };
 
